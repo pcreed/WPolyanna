c05e72361e688b35540ab7923c342c467b42d768
Paidi Creed <paidi_creed@yahoo.co.uk>
2012-04-16 17:14:53 +0100

    Added init files

---
+++ wpolyanna/__init__.py
@@ -0,0 +1,10 @@
+from op import Operation, ExplicitOperation, Projection
+from binop import BinaryOperation
+from clone import Clone
+from affine import AffineOperation
+from semiprojection import Semiprojection
+import wop
+from wop import WeightedOperation
+import cost_function
+from cost_function import CostFunction
+from cost_function import wpol
---
+++ wpolyanna/test/__init__.py
@@ -0,0 +1,6 @@
+from wpolyanna.test.test_op import *
+from wpolyanna.test.test_binop import *
+from wpolyanna.test.test_clone import *
+from wpolyanna.test.test_wop import *
+from wpolyanna.test.test_cost_function import *
+from wpolyanna.test.test_submodular import *
